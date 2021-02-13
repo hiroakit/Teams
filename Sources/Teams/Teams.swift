f93af98fa5f072e44721f2de6a019f3365264723
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2021-02-13 16:22:59 +0900

    Add empty method

--- Sources/Teams/Teams.swift
+++ Sources/Teams/Teams.swift
@@ -1,3 +1,11 @@
 public struct Teams {
-   public var text = "Hello, World!"
+    public var text = "Hello, World!"
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    public func empty() -> Teams {
+        return Teams(text: "")
+    }
 }
