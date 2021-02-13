c6f0b3174600016205f1eab28b98a618f927dcfa
Hiroaki ENDOH <hiroakiendoh@gmail.com>
2021-02-13 16:09:05 +0900

    Add platform version

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Teams",
+    platforms: [.macOS(.v10_14),
+                .iOS(.v14),
+                .tvOS(.v14),
+                .watchOS(.v5)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
